c9da422675e968baef5fd34d020332b54a34e505
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-20 10:50:02 +1400

    00 UPDATE

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,55 +12,74 @@ let minutesV = document.querySelector("span[data-minutes]");
 let secondsV = document.querySelector("span[data-seconds]");
 
 const dateToday = new Date();
-let diff;
-let value;
-
-startBtn.addEventListener('click', () => {
-  continueInterval();
-  startBtn.disabled = true;
-  });
-
-countDownTimer();
-let timerId = setInterval(countDownTimer, 1000);
+var diff;
+var value;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {  
+  onClose(selectedDates, countDownTimer) {  
     value = selectedDates[0];
     if (value < dateToday) {
       window.alert(`Please choose a date in the future`);
-      startBtn.disabled = true;
       } else {
-      startBtn.disabled = false;
+        startBtn.disabled = false;
+        startBtn.addEventListener('click', () => {
+        let timerId = setInterval(countDownTimer, 1000);
+        startBtn.disabled = true;
+        });
+      
+        countDownTimer();
+        
+        function countDownTimer() {
+        const today = new Date();
+        diff = value - today;    
+      
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+      
+        const days = Math.floor(diff / day);
+        const hours = Math.floor((diff % day) / hour);
+        const minutes = Math.floor(((diff % day) % hour) / minute);
+        const seconds = Math.floor((((diff % day) % hour) % minute) / second);
+      
+        daysV.textContent = `${days}`;
+        hoursV.textContent = `${hours}`;
+        minutesV.textContent = `${minutes}`;
+        secondsV.textContent = `${seconds}`;
+    
+       
+        if (diff <= 0) {
+          stopInverval();
+          alert('The day has come! :)');
+             }
+             let o = daysV.textContent;
+addLeadingZero(o);
+             
+        function addLeadingZero() {
+          if (daysV.textContent < 10 || minutesV.textContent < 10 || 
+              secondsV.textContent < 10  || hoursV.textContent < 10 ) {
+            
+            let paddedDays = daysV.textContent.padStart(2,"0");
+             paddedHours = hoursV.textContent.padStart(2,"0");
+             paddedMin = minutesV.textContent.padStart(2,"0");
+             paddedSec = secondsV.textContent.padStart(2,"0");
+            daysV.textContent = paddedDays;
+            hoursV.textContent = paddedHours;
+            minutesV.textContent = paddedMin;
+            secondsV.textContent = paddedSec;
+          
+        }
+        
+        }
       }
-}};      
- 
-function continueInterval() {
-  timerId = setInterval(countDownTimer, 1000);
-  }
+      }
+  }    
+};   
 
-function countDownTimer() {
-  diff = value - dateToday;        
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(diff / day);
-  const hours = Math.floor((diff % day) / hour);
-  const minutes = Math.floor(((diff % day) % hour) / minute);
-  const seconds = Math.floor((((diff % day) % hour) % minute) / second);
-
-  daysV.textContent = `${days}`;
-  hoursV.textContent = `${hours}`;
-  minutesV.textContent = `${minutes}`;
-  secondsV.textContent = `${seconds}`;
-
-  console.log(value); 
-} 
 
 flatpickr(input, options);
