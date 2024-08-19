08dd5d11cda25c891b8ea2e12615468ac00a2104
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-20 12:09:19 +1400

    finalizare tema

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@
 'use strict';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const input = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector("button[data-start]");
@@ -14,6 +15,7 @@ let secondsV = document.querySelector("span[data-seconds]");
 const dateToday = new Date();
 var diff;
 var value;
+let timerId;
 
 const options = {
   enableTime: true,
@@ -23,12 +25,16 @@ const options = {
   onClose(selectedDates, countDownTimer) {  
     value = selectedDates[0];
     if (value < dateToday) {
-      window.alert(`Please choose a date in the future`);
+      Notiflix.Notify.failure(`Please choose a date in the future`);
+       startBtn.disabled = true;
       } else {
         startBtn.disabled = false;
         startBtn.addEventListener('click', () => {
-        let timerId = setInterval(countDownTimer, 1000);
+        timerId = setInterval(countDownTimer, 1000);
         startBtn.disabled = true;
+        input.flatpickr({
+          enable: [value]
+      });
         });
       
         countDownTimer();
@@ -54,32 +60,28 @@ const options = {
     
        
         if (diff <= 0) {
-          stopInverval();
-          alert('The day has come! :)');
-             }
-             let o = daysV.textContent;
-addLeadingZero(o);
+          clearInterval(timerId);
+          Notiflix.Notify.success(`The day has come! :)`);
+         }
+        
+        addLeadingZero();
              
         function addLeadingZero() {
           if (daysV.textContent < 10 || minutesV.textContent < 10 || 
-              secondsV.textContent < 10  || hoursV.textContent < 10 ) {
+            secondsV.textContent < 10  || hoursV.textContent < 10 ) {
             
             let paddedDays = daysV.textContent.padStart(2,"0");
-             paddedHours = hoursV.textContent.padStart(2,"0");
-             paddedMin = minutesV.textContent.padStart(2,"0");
-             paddedSec = secondsV.textContent.padStart(2,"0");
+            paddedHours = hoursV.textContent.padStart(2,"0");
+            paddedMin = minutesV.textContent.padStart(2,"0");
+            paddedSec = secondsV.textContent.padStart(2,"0");
             daysV.textContent = paddedDays;
             hoursV.textContent = paddedHours;
             minutesV.textContent = paddedMin;
             secondsV.textContent = paddedSec;
-          
-        }
-        
+          }}      
         }
       }
-      }
-  }    
-};   
-
-
+    }    
+  };   
+  
 flatpickr(input, options);
