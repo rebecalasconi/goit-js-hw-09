e281e005ccfd33e9f132e217099bcc5698423503
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-20 08:32:08 +1400

    update timer diff

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,69 +5,61 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const input = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector("button[data-start]");
+
 let daysV = document.querySelector("span[data-days]");
 let hoursV = document.querySelector("span[data-hours]");
 let minutesV = document.querySelector("span[data-minutes]");
 let secondsV = document.querySelector("span[data-seconds]");
+
 const dateToday = new Date();
+let diff;
+let value;
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-       let value = selectedDates[0];
-         if (value < dateToday) {
-         window.alert(`Please choose a date in the future`);
-         startBtn.disabled = true;
-        } else {
-         startBtn.disabled = false;
-        }
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {  
+    value = selectedDates[0];
+    if (value < dateToday) {
+      window.alert(`Please choose a date in the future`);
+      startBtn.disabled = true;
+      } else {
+      startBtn.disabled = false;
+      }
   
-    let diff = value - dateToday;    
-    let timerId = setInterval(countDownTimeToNY, 1000);
-                 
-    startBtn.addEventListener('click', () => {
+  startBtn.addEventListener('click', () => {
     continueInterval();
     startBtn.disabled = true;
     });
 
-
-
-
-                
-    function continueInterval() {
-    timerId = setInterval(countDownTimeToNY, 1000);
+  countDownTimer();
+  let timerId = setInterval(countDownTimer, 1000);
+   
+  function continueInterval() {
+    timerId = setInterval(countDownTimer, 1000);
     }
 
-    function countDownTimeToNY() {
-           
+  function countDownTimer() {
+    diff = value - dateToday;        
+
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-
   const days = Math.floor(diff / day);
-
   const hours = Math.floor((diff % day) / hour);
-
   const minutes = Math.floor(((diff % day) % hour) / minute);
-
   const seconds = Math.floor((((diff % day) % hour) % minute) / second);
 
- daysV.textContent = `${days}`;
- hoursV.textContent = `${hours}`;
- minutesV.textContent = `${minutes}`;
- secondsV.textContent = `${seconds}`;
-
- console.log(diff);
- return { daysV, hoursV, minutesV, secondsV };
-
-} countDownTimeToNY();
+  daysV.textContent = `${days}`;
+  hoursV.textContent = `${hours}`;
+  minutesV.textContent = `${minutes}`;
+  secondsV.textContent = `${seconds}`;
+  console.log(diff); 
+} 
 }};      
-                
-
-
+              
 flatpickr(input, options);
